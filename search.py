f629bcd1500f6c12d63d15809f53f832111883bc
namenok <comfykh810@gmail.com>
2025-11-03 18:30:19 +0200

    4-LLM added

--- search.py
+++ search.py
@@ -1,3 +1,4 @@
+import ollama
 import json
 import numpy as np
 from sentence_transformers import SentenceTransformer
@@ -13,7 +14,6 @@ with open('documents.json', 'r', encoding='utf-8') as f:
 embeddings = np.load('embeddings.npy')
 
 print(f"Database loaded successfully ({len(documents)} documents).")
-
 
 def find_top_k(query_text, k=3):
 
@@ -44,3 +44,22 @@ for i, result in enumerate(top_results):
     print(f"Rank {i+1} (Score: {result['score']:.4f}):")
     print(result['text'])
     print("-------------------")
+
+context_texts = [result['text'] for result in top_results]
+my_glue = "\n\n"
+
+context = my_glue.join(context_texts)
+prompt = f'use only this contex: {context}. Give an answear on this question: {my_query}'
+
+print("\n--- Sending to LLM ---")
+
+response = ollama.chat(model='llama3', messages=[
+    {
+        'role': 'user',
+        'content': prompt,
+    }
+])
+
+print(response)
+ollama_answer = str(response['message']['content'])
+print(ollama_answer)
